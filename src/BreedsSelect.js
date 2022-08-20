8c8478c2ce3f9a7aa8955950f0bd31a88db5852c
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-08-20 20:30:41 +0900

    selectedBreed

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -2,15 +2,17 @@
 import * as React from "react";
 
 const BreedsSelect = (props) => {
-  const handleChange = async (e) => {
-    await props.setSelectedBreed(e.target.value)
+  const handleChange = (e) => {
+    props.setSelectedBreed(e.target.value)
     console.log(props.selectedBreed)
   }
   return (
     <div>
       <select
         onChange={(e) => handleChange(e)}
+        value={props.selectedBreed}
       >
+        <option value="">all</option>
         {props.breeds.map((breed) => {
           return (
             <option value={breed}>
