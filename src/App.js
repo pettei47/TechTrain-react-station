f179378981fd5d97f2aa6cb4d5eb0251c93eff7f
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-08-20 14:45:20 +0900

    04

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/redbone/n02090379_4138.jpg")
+  // setDogUrl("https://images.dog.ceo/breeds/redbone/n02090379_4138.jpg")
   return (
     <div>
       <header><h1>DogApp</h1></header>
       <main>
-        <img src="https://images.dog.ceo/breeds/redbone/n02090379_4138.jpg" />
+        <img src={dogUrl} />
       </main>
       かわいい犬を眺めましょう
     </div>
